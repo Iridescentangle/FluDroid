11689a2da73244c4eeda77b3a732d6563384a6b3
17611420051@163.com <17611420051@163.com>
2019-01-11 19:01:28 +0800

    11

--- lib/tec_page.dart
+++ lib/tec_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:sky_engine/_http/http.dart' as http;
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:ui';
 class TecPage extends StatefulWidget {
   _TecPageState createState() => _TecPageState();
 }
@@ -11,10 +16,19 @@ class _TecPageState extends State<TecPage> {
   var pageUrl ;
   @override
     void initState() {
-      // TODO: implement initState
       super.initState();
       pageUrl = "http://www.wanandroid.com/article/list/${page}/json";
+      loadData(page);
     }
+  void loadData(int page){
+    var client = http.HttpClient();
+    // var request =await client.getUrl(pageUrl);
+    // // var request = await httpClient.getUrl(uri1);
+    // var response = await request.close();
+    // var responseBody = await response.transform(utf8.decoder).join();
+    // var result = json.decode(responseBody);
+    // print(result)
+  }
   @override
   Widget build(BuildContext context) {
     // if(_body_list != null || _body_list.length <= 0){
