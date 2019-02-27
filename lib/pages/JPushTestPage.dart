64b2bb90cee22676b4b62df0cf6fb6964180a612
Iridescentangle <17611420051@163.com>
2019-02-27 20:00:32 +0800

    测试SSH

--- lib/pages/JPushTestPage.dart
+++ lib/pages/JPushTestPage.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+class JPushTestPage extends StatefulWidget {
+  final Widget child;
+
+  JPushTestPage({Key key, this.child}) : super(key: key);
+
+  _JPushTestPageState createState() => _JPushTestPageState();
+}
+
+class _JPushTestPageState extends State<JPushTestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: widget.child,
+    );
+  }
+}
